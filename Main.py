8408ad6c49e889c8f92da727f46d58d7acc547a0
Screenbones <whoami@kotsu.red>
2024-04-02 20:09:50 -0700

    Add Sky Conditions

--- Main.py
+++ Main.py
@@ -3,17 +3,29 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
-place = st.text_input("Place: ")
+place = st.text_input("Place: ", value="Tokyo")
 days = st.slider("Forecast Days", min_value=1, max_value=5, 
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+filtered_data = get_data(place, days)
 
-dates = ["2005-10-24", "2005-10-25", "2005-10-26"] # Placeholder
-temperatures = [200, 300, 400] # Placeholder
 labels = {"x": "Date", "y": "Temperature (C)"}
 
-figure = px.line(x=dates, y=temperatures, labels=labels)
-st.plotly_chart(figure)
+if option == "Temperature":
+    temperatures = [dict["main"]["temp"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    figure = px.line(x=dates, y=temperatures, labels=labels)
+    st.plotly_chart(figure)
+
+if option == "Sky":
+    sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+    image_paths = {
+        "Clear": "images/clear.png",
+        "Clouds": "images/cloud.png",
+        "Rain": "images/rain.png",
+        "Snow": "images/snow.png"
+    }
+    images = [image_paths[condition] for condition in sky_conditions]
+    st.image(images, width=115)
--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import os
 import requests
 API_KEY = os.getenv("WEATHER_API")
 
-def get_data(place="Tokyo", days=3, kind="Temperature"):
+def get_data(place="Tokyo", days=3):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}" \
         f"&appid={API_KEY}"
     response = requests.get(url)
@@ -10,10 +10,6 @@ def get_data(place="Tokyo", days=3, kind="Temperature"):
     filtered_data = data["list"]
     values = 8 * days
     filtered_data = filtered_data[:values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
