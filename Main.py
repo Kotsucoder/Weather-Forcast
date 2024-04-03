5b162390f2ebdf5309bcf8bf6138c271fb027a90
Screenbones <whoami@kotsu.red>
2024-04-02 20:14:21 -0700

    Fix the Weather Forecast App Student Project

--- Main.py
+++ Main.py
@@ -9,23 +9,26 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-filtered_data = get_data(place, days)
+try:
+    filtered_data = get_data(place, days)
 
-labels = {"x": "Date", "y": "Temperature (C)"}
+    labels = {"x": "Date", "y": "Temperature (C)"}
 
-if option == "Temperature":
-    temperatures = [dict["main"]["temp"] for dict in filtered_data]
-    dates = [dict["dt_txt"] for dict in filtered_data]
-    figure = px.line(x=dates, y=temperatures, labels=labels)
-    st.plotly_chart(figure)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels=labels)
+        st.plotly_chart(figure)
 
-if option == "Sky":
-    sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-    image_paths = {
-        "Clear": "images/clear.png",
-        "Clouds": "images/cloud.png",
-        "Rain": "images/rain.png",
-        "Snow": "images/snow.png"
-    }
-    images = [image_paths[condition] for condition in sky_conditions]
-    st.image(images, width=115)
+    if option == "Sky":
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = {
+            "Clear": "images/clear.png",
+            "Clouds": "images/cloud.png",
+            "Rain": "images/rain.png",
+            "Snow": "images/snow.png"
+        }
+        images = [image_paths[condition] for condition in sky_conditions]
+        st.image(images, width=115)
+except KeyError:
+    st.write("This place does not exist.")
